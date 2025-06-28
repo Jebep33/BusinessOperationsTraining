74cd6d2cb08bf2be1b9dec47a3df80f38dc76933
Jebep8933 <87921319+Jebep33@users.noreply.github.com>
2025-06-28 14:11:43 +0800

    Added badParse.py

---
+++ Week3/badParse.py
@@ -0,0 +1,11 @@
+import requests
+
+try:
+    response = requests.get("https://example.com")
+    response.raise_for_status()
+    data = response.json()
+except requests.exceptions.RequestException as e:
+    print("Network Error:", e)
+except ValueError:
+    print("Error: The response is not valid JSON.")
+    
