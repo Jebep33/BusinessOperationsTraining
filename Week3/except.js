e0a877e0e8ef99d2999fa063faada68e2fa9d0a9
Jebep8933 <87921319+Jebep33@users.noreply.github.com>
2025-08-13 17:17:47 +0800

    corrected code

--- Week3/except.js
+++ Week3/except.js
@@ -1,21 +1,25 @@
-function divideNumbers(a,b){
+let a = 1;
+let b = "Test";
+
+function divideNumbers(){
     return a/b;
 }
 
 try{
-    divideNumbers(1,"Jerome");
+    var res = divideNumbers();
 
     if (b === 0){
-        throw new Error("Math error: Cannot divide by zero.")
+        throw new Error("Math error: Cannot divide by zero.");
     }
-    else if(isNaN(divideNumbers())){
-        throw new Error("Input error: Please input a number.")
+    else if(isNaN(divideNumbers(a,b))){
+        throw new Error("Input error: Please input a number.");
     }
+
+    console.log(res);
 }
 catch(e){
     console.log(e);
-    console.log( "You cannot divide by zero.");
 }
-finally{
+/*finally{
     console.log("Please try again.");
-}
+}*/
