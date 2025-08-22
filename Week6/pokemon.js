a5cd8d3a5a42ea5be558400001664f0155a8e271
Jebep33 <87921319+Jebep33@users.noreply.github.com>
2025-08-22 17:11:40 +0800

    Added pokemon.html and js

---
+++ Week6/pokemon.js
@@ -0,0 +1,12 @@
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.name);
+        console.log(data.abilities[0].ability.name);
+        console.log(data.sprites.front_default);
+
+        let image = document.createElement("img");
+        image.src = data.sprites.front_default;
+        document.body.appendChild(image);
+    })
+
