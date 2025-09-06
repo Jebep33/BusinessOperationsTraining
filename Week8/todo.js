e70a06c0139c9939ee9adaa1ee3393b0362d76f3
Jebep8933 <87921319+Jebep33@users.noreply.github.com>
2025-09-06 19:11:49 +0800

    Added Week 8 Activity

---
+++ Week8/todo.js
@@ -0,0 +1,30 @@
+// ############# ACTIVITY 1 ###############
+
+/*fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(res => res.json())
+  .then(data => {
+    let incomplete = data.filter(task => task.completed === false);
+    console.log("Incomplete Tasks:", incomplete);
+    console.log("Number of Results: ", incomplete.length);
+  });*/
+
+// ############## ACTIVITY 2 #################
+
+  fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(res => res.json())
+  .then(data => {
+    let userTaskCount = {};
+
+    data.forEach(datum => {
+        userTaskCount[datum.userId] = (userTaskCount[datum.userId] || 0) + 1;
+    });
+
+    let activeUsers = Object.keys(userTaskCount).filter(userID => userTaskCount[userID] > 3);
+    console.log("Users with more than 3 tasks:", activeUsers);
+    console.log("User Task Counts: ", userTaskCount);
+
+    let filteredToDos = data.filter(todo => activeUsers.includes(String(todo.userId)));
+    //console.log("Number of Results:",filteredToDos.length);
+    //console.log(filteredToDos);
+  });
+
